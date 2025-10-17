804056c0bf2c2d57c0eb8b4266f1dada08208837
haleyprindle <haley@prindle.net>
2025-10-17 11:44:31 -0400

    Create PetSimulator.java

---
+++ PetSimulator.java
@@ -0,0 +1,49 @@
+import java.util.*;
+public class Pet{
+    public static void main(String [] args){
+        //code goes here
+        Scanner input = new Scanner(System.in);
+
+        String pet;
+        System.out.println("Enter your favorite color (Either red, blue or green): ");
+        String favoriteColor = input.nextLine();
+        System.out.println("Enter your favorite season (winter, spring, summer, fall): ");
+        String favoriteSeason = input.nextLine();
+        System.out.println("Enter your name: ");
+        String name = input.nextLine();
+        String consonant = "bcdfghjklmnpqrstvwxyz"
+        boolean isThereAConsonant = false
+         if(name[0] != "a" && s != "e" && s != "i" && s != "o" && s != "u")
+        {
+            isConsonant = true;
+        } else {
+            isConsonant = false;
+        }
+
+
+          if (favoriteColor == "blue")
+          {
+            if (favoriteSeason == "fall")
+            {
+                String pet == "alligator";
+            }
+            else
+            {
+                if (favoriteSeason == "spring")
+                {
+                    String pet == "ostritch";
+                }
+                else
+                {
+                    if (favoriteSeason != "summer" && isThereAConsonant == true)
+                    {
+                        if (pet != "ostritch")
+                        {
+                            pet == "axolotl"
+                        }
+                    }
+                }
+            }
+          }
+    }
+}
