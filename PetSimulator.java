1483a75fd06f6fa3aa88238fced80dc687f619e8
haleyprindle <haley@prindle.net>
2025-10-18 01:18:49 +0000

    Finished

--- PetSimulator.java
+++ PetSimulator.java
@@ -1,47 +1,71 @@
+//Haley Prindle 
+//10/17/25
+//Description: Accurately calculates the proper pet for anyone
 import java.util.*;
 public class PetSimulator{
     public static void main(String [] args){
         //code goes here
         Scanner input = new Scanner(System.in);
-
+        String[] validColors = {"red", "blue", "green"};
+        String[] validSeasons = {"winter", "spring", "summer", "fall"};
         String pet = "";
-        System.out.println("Enter your favorite color (Either red, blue or green): ");
-        String favoriteColor = input.nextLine().toLowerCase();
-        System.out.println("Enter your favorite season (winter, spring, summer, fall): ");
-        String favoriteSeason = input.nextLine().toLowerCase();
-        System.out.println("Enter your name: ");
-        String name = input.nextLine().toLowerCase();
+        boolean valid= false;
+        String favoriteColor =null;
+
+        while(!valid) { // loop until you get a valid color
+            System.out.println("Enter your favorite color (Either red, blue or green): ");
+             favoriteColor = input.nextLine().toLowerCase();
+            for(int j=0;j<validColors.length;j++) // loop over valid colors to see if there is a match
+            {
+                if(validColors[j].equals(favoriteColor)){
+                    valid = true; // stop prompting the user for another color
+                    break; // found it so stop checking the rest of the colors
+                }
+            }
+    }
+            valid = false;
+            String favoriteSeason = null;
+            while(!valid) {
+            System.out.println("Enter your favorite season (winter, spring, summer, fall): ");
+            favoriteSeason = input.nextLine().toLowerCase();
+            for(String s :validSeasons){ 
+                if(s.equals(favoriteSeason)){
+                    valid = true;
+                    break;
+                }
+            }
+    }
+        
+String name = "";
+           
+     
+        valid = false;
+        while(!valid){
+            System.out.println("Enter your name: ");
+            name = input.nextLine().toLowerCase();
+            if(name.length() == 0) // check for empty string
+                continue; // User entered "", so restart loop to prompt again
+            
+            int i = 0;
+            valid = true; // Start with assumption that the input is valid
+        while (i < name.length()) { // iterate through all chars and check that each is a letter
+                String s = name.substring(i, i + 1);
+            if ("abcdefghijklmnopqrstuvwxyz".indexOf(s) < 0) // check if character is invalid
+            {
+                System.out.println ("Please input letters only.");  
+                valid   = false; // we don't have a valid input, so start prompt loop again
+                break;  // Found one bad character, no need to check the rest
+            }   
+            i++; // check the next character
+
+        }
+    }
+
         input.close();
+    
         boolean isThereAConsonant;
-        String firstLetter = name.substring(0,1);
-        int i = 0;
-
-        while (++i < name.length()) {
-        if ("abcdefghijklmnopqrstuvwxyz".indexOf(name.substring(i, i + 1).toLowerCase()) < 0) 
-        {
-          System.out.println ("Please input letters only.");  
-              return;  
-        }  //this is a letter
-                
-        }
-
-        while (i++ < favoriteColor.length()) {
-        if ("abcdefghijklmnopqrstuvwxyz".indexOf(name.substring(i, i + 1).toLowerCase()) < 0) 
-        {
-          System.out.println ("Please input letters only."); 
-              return;  
-        }  //this is a letter
-                
-        }
-
-        while (i < favoriteSeason.length()) {
-        if ("abcdefghijklmnopqrstuvwxyz".indexOf(name.substring(i, i + 1).toLowerCase()) < 0) 
-        {
-         System.out.println ("Please input letters only.");
-              return;
-        }  //this is a letter
-                
-        }
+        String firstLetter = "";
+        if(name.length()>0) firstLetter = name.substring(0,1);
 
          if(!firstLetter.equals("a") &&  !firstLetter.equals("e") && !firstLetter.equals("i") && !firstLetter.equals("o") && !firstLetter.equals("u"))
         {
@@ -61,7 +85,7 @@ public class PetSimulator{
             {
                 if (favoriteSeason.equals("spring"))
                 {
-                    pet = "ostritch";
+                    pet = "ostrich";
                 }
                 else
                 {
@@ -69,7 +93,7 @@ public class PetSimulator{
                     {
                         if (!favoriteSeason.equals("fall"))
                         {
-                            if (!pet.equals("ostritch") )
+                            if (!pet.equals("ostrich") )
                             {
                             pet = "axolotl";   
                             }
@@ -82,7 +106,7 @@ public class PetSimulator{
 
           if(favoriteColor.equals("green"))
           {
-            if(isThereAConsonant == true && favoriteSeason.equals("winter"));
+            if(isThereAConsonant == true && favoriteSeason.equals("winter"))
             {
                 pet = "giraffe";
             }
